67756ec7295394726a99927749175e7d5aaa8816
ESSOWE JUSTIN BAKOUBOLO <71730008+Justobak01@users.noreply.github.com>
2020-11-28 14:59:47 +0100

    Update Source.c

--- Exercice2/Source.c
+++ Exercice2/Source.c
@@ -4,9 +4,9 @@
 #define TAILLEINITIALE 100
 
 typedef struct Tableau {
-	int *elt; // le tableau díentiers
-	int size; // la taille de ce tableau díentiers
-	int eltsCount; // le nombre díÈlÈments dans le tableau
+	int *elt; // le tableau d‚Äôentiers
+	int size; // la taille de ce tableau d‚Äôentiers
+	int eltsCount; // le nombre d‚Äô√©l√©ments dans le tableau
 } TABLEAU;
 
 TABLEAU newArray (){
@@ -32,7 +32,7 @@ int setElement(TABLEAU* tab, int pos, int element) {
 	if (pos > (tab->size)) {
 		int *temp; 
 		temp = tab->elt;
-		int newsize = (tab->size) + (pos - (tab->size)); // Ajout de l'ecart entre pos et size ‡ taille
+		int newsize = (tab->size) + (pos - (tab->size)); // Ajout de l'ecart entre pos et size √† taille
 		tab->elt = (int*) realloc (tab->elt, newsize*sizeof(int));
 		if (tab->elt== NULL) {
 			tab->elt = temp;
@@ -41,7 +41,7 @@ int setElement(TABLEAU* tab, int pos, int element) {
 		for (int i = (tab->size); i < pos-1; i++) { 
 			*(tab->elt +i) = 0;
 		}
-		*(tab->elt +pos-1) = element; // tab->elt est ‡ la position 1 et ‡ l'indice 0 du tableau donc pos correspond ‡ l'indice pos-1 du tableau.
+		*(tab->elt +pos-1) = element; // tab->elt est √† la position 1 et √† l'indice 0 du tableau donc pos correspond √† l'indice pos-1 du tableau.
 		(tab->eltsCount) += 1;
 		(tab->size)= newsize;
 		return pos;
@@ -52,7 +52,7 @@ int setElement(TABLEAU* tab, int pos, int element) {
 }
 
 int displayElements(TABLEAU* tab, int startPos, int endPos) {
-	if (tab == NULL || startPos <= 0 || endPos <= 0 || startPos > (tab->size) /*|| endPos > (tab->size)*/) {
+	if (tab == NULL || startPos <= 0 || endPos <= 0 || startPos > (tab->size) || endPos > (tab->size)) {
 		return (-1);
 	}
 	if (startPos>= endPos)
@@ -77,7 +77,7 @@ int deleteElements(TABLEAU* tab, int startPos, int endPos) {
 		endPos = temp;
 	}
 	
-	for (int i = 1 ; i< (endPos - startPos);i++){ // On recule le tableau d'une valeur Ècart ‡ partir de startPos et endPos; les positions startPos et endPos sont aussi supprimÈe.
+	for (int i = 1 ; i< (endPos - startPos);i++){ // On recule le tableau d'une valeur √©cart √† partir de startPos et endPos; les positions startPos et endPos sont aussi supprim√©e.
 		*(tab->elt + startPos + i - 1) = *(tab->elt + endPos + i);
 	}
 	int newsize = tab->size - (endPos - startPos);
@@ -90,16 +90,16 @@ int deleteElements(TABLEAU* tab, int startPos, int endPos) {
 
 int main() {
 	TABLEAU Test = newArray();
-	for (int i = 0; i < (Test.size); i++) { // initialisation des ÈlÈment ‡ 0;
+	for (int i = 0; i < (Test.size); i++) { // initialisation des √©l√©ment √† 0;
 		*(Test.elt + i) = 0;
 	}
-	int position1 = setElement(&Test, 50, 45); // On remarque que 45 est affichÈ ‡ la position 50
+	int position1 = setElement(&Test, 50, 45); // On remarque que 45 est affich√© √† la position 50
 	
 	if (displayElements(&Test, 30, 50) >=0) {
 		printf("\n\n");
 	}
-	if (incrementArraySize(&Test, 50) >= 0) { // Lors du test on remarque que cette fonction n'arrive pas ‡ modifier la taille du TABLEAU, 
-		printf("\n\n");                      // cela est probablement d˚ au fait que Tailleinitiale est dÈcalrÈ en Macro 
+	if (incrementArraySize(&Test, 50) >= 0) { // Lors du test on remarque que cette fonction n'arrive pas √† modifier la taille du TABLEAU, 
+		printf("\n\n");                      // cela est probablement d√ª au fait que Tailleinitiale est d√©calr√© en Macro 
 	}  
 	if (displayElements(&Test, 100, 150) == 0) {
 		printf("\n\n");
